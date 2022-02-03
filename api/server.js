2681ff41ecaa8291f86dff3ad7b423574b75aa08
Jacob Brooks <myrefugemyfortress@yahoo.com>
2022-02-02 22:25:53 -0500

    Starting Repo

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+const helmet = require("helmet")
+
+const authRouter = require('./auth/auth-router')
+const itemsRouter = require('./items/items-router')
+
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
+server.use('/api/auth', authRouter)
+server.use('/api/items', itemsRouter);
+
+server.get("/",(req, res) =>{
+    res.json({"Server up it is"})
+})
+module.exports = server;
